0dbb3d587d17c8b9a1de6fefe53f4ae7ca77e193
DavidTG7 <davidthekoa@gmail.com>
2023-09-08 22:20:43 -0500

    Add logic to handle behaviour when left clicking

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { createBoard, markTile, TILE_STATUS } from "./minesweeper.js";
+import {
+  createBoard,
+  markTile,
+  revealTile,
+  TILE_STATUS,
+} from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
@@ -6,6 +11,11 @@ const NUMBER_OF_MINES = 10;
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElement = document.querySelector(".board");
 const mineCounter = document.querySelector(".mine-counter");
+const restartButton = document.querySelector(".restart-button");
+
+restartButton.addEventListener("click", () => {
+  location.reload();
+})
 
 boardElement.style.setProperty("--size", BOARD_SIZE);
 mineCounter.textContent = NUMBER_OF_MINES;
@@ -13,7 +23,9 @@ mineCounter.textContent = NUMBER_OF_MINES;
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.cell);
-    tile.cell.addEventListener("click", () => {});
+    tile.cell.addEventListener("click", () => {
+      revealTile(board, tile);
+    });
     tile.cell.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       markTile(tile);
@@ -24,9 +36,10 @@ board.forEach((row) => {
 
 const listMinesLeft = () => {
   const markedTilesCount = board.reduce((count, row) => {
-    return count + row.filter((tile) => tile.status === TILE_STATUS.MARKED).length;
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUS.MARKED).length
+    );
   }, 0);
-
 
   mineCounter.textContent = NUMBER_OF_MINES - markedTilesCount;
 };
