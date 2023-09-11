bf78aba9884214a14ccf624f4719ccc35eb3e5b5
DavidTG7 <davidthekoa@gmail.com>
2023-09-10 22:19:54 -0500

    Add logic to the count mines left

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-import { createBoard } from "./minesweeper.js";
+import { createBoard, markTile, TILE_STATUS } from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElement = document.querySelector(".board");
+const mineCounter = document.querySelector(".mine-counter");
 
 boardElement.style.setProperty("--size", BOARD_SIZE);
+mineCounter.textContent = NUMBER_OF_MINES;
 
 board.forEach((row) => {
   row.forEach((tile) => {
-    if (tile.mine) tile.cell.style.background = "green";
     boardElement.append(tile.cell);
+    tile.cell.addEventListener("click", () => {});
+    tile.cell.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
   });
 });
+
+const listMinesLeft = () => {
+  const markedTilesCount = board.reduce((count, row) => {
+    return count + row.filter((tile) => tile.status === TILE_STATUS.MARKED).length;
+  }, 0);
+
+
+  mineCounter.textContent = NUMBER_OF_MINES - markedTilesCount;
+};
