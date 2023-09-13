d244f25ba84691be6400281e04223cd686415e17
DavidTG7 <davidthekoa@gmail.com>
2023-09-12 23:13:09 -0500

    Complete logic to win or lose checking

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ import {
   markTile,
   revealTile,
   TILE_STATUS,
+  checkWin,
+  checkLose,
 } from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
-const NUMBER_OF_MINES = 10;
+const NUMBER_OF_MINES = 15;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElement = document.querySelector(".board");
 const mineCounter = document.querySelector(".mine-counter");
 const restartButton = document.querySelector(".restart-button");
+const messageText = document.querySelector(".subtext");
 
 restartButton.addEventListener("click", () => {
   location.reload();
-})
+});
 
 boardElement.style.setProperty("--size", BOARD_SIZE);
 mineCounter.textContent = NUMBER_OF_MINES;
@@ -25,6 +28,7 @@ board.forEach((row) => {
     boardElement.append(tile.cell);
     tile.cell.addEventListener("click", () => {
       revealTile(board, tile);
+      checkGameEnd();
     });
     tile.cell.addEventListener("contextmenu", (e) => {
       e.preventDefault();
@@ -42,4 +46,36 @@ const listMinesLeft = () => {
   }, 0);
 
   mineCounter.textContent = NUMBER_OF_MINES - markedTilesCount;
+};
+
+const checkGameEnd = () => {
+  const win = checkWin(board);
+  const lose = checkLose(board);
+
+  if (win || lose) {
+    boardElement.addEventListener("click", stopProp, { capture: true });
+    boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+  }
+
+  if (win) {
+    messageText.style.background = "green"
+    messageText.style.padding = "0 10px"
+    messageText.textContent = "You Win :D";
+  }
+
+  if (lose) {
+    messageText.style.background = "darkred"
+    messageText.style.padding = "0 10px"
+    messageText.textContent = "You Lose :(";
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUS.MARKED) markTile(tile);
+        if (tile.mine) revealTile(board, tile);
+      });
+    });
+  }
+};
+
+const stopProp = (e) => {
+  e.stopImmediatePropagation();
 };
